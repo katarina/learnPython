03108ec7779889251841939e363166c9f48c337c
Katarina <katarina.loughlin@gmail.com>
2012-05-01 00:07:39 -0400

    Exercise 32: Loops And Lists

---
+++ Exercises/Ex32/ex32.py
@@ -0,0 +1,41 @@
+#Exercise 32: loops and lists
+
+haris = ['brown', 'blond', 'red']
+eyes = ['brown', 'blue', 'green']
+weights = [1,2,3,4]
+
+the_count = [1,2,3,4,5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3,'quarters']
+
+#this first kind for for-loop goes through a list
+for number in the_count:
+	print "This is count %d" % number
+
+#same as above
+for fruit in fruits:
+	print "a fruit of type: %s" % fruit
+
+#also we can go through mixed lists too
+#notice we have to use %r since we don't know what's in it
+for i in change:
+	print "I got %r" % i
+
+#we can also build lists, first start with an empty one
+elements = []
+
+#then use the range function to do 0 to 4 counts
+#for i in range(0,6):
+#	print "Adding %d to the list." % i
+#	#append is a function that lsits understand
+#	elements.append(i)
+
+#now we can print them out too
+#for i in elements:
+#	print "Element was: %d" % i
+print "I AM THINGS"
+
+for i in range(0,6):
+	elements.append(i)
+	print i
+
